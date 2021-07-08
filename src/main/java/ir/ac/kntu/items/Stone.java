a3be098962bbca9b57dadbcd4f8f07379b02c0b9
MrShadowKiller <m.jooker2016@gmail.com>
2021-07-08 22:35:56 +0430

    add Stone

---
+++ src/main/java/ir/ac/kntu/items/Stone.java
@@ -0,0 +1,10 @@
+package ir.ac.kntu.items;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Stone extends Block{
+    public Stone() {
+        super(new ImageView("assets/gravel.png"));
+    }
+}
